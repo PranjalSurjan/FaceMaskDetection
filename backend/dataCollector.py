a37ce8e7ed850729d1db2b860e036138ff4e5a25
PranjalSurjan <Pranjalsurjan03@gmail.com>
2025-07-03 07:52:34 +0000

    added middleware cors, fastapi

--- backend/dataCollector.py
+++ backend/dataCollector.py
@@ -2,7 +2,18 @@ from fastapi import FastAPI, File, UploadFile
 import cv2
 import numpy as np
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or specify frontend URL in prod
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.post("/detect-mask/")
 async def detect_mask(file: UploadFile = File(...)):
